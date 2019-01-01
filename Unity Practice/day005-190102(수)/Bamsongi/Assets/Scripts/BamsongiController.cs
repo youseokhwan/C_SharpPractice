4bd3e2c87e6594776b0bec94c37101f4c977f64d
youseokhwan <tjrghks115@naver.com>
2019-01-02 01:17:42 +0900

    Practice Unity: Terraian, ParticleSystem

---
+++ Unity Practice/day005-190102(수)/Bamsongi/Assets/Scripts/BamsongiController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BamsongiController : MonoBehaviour {
+    public void Shoot(Vector3 dir) {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        GetComponent<Rigidbody>().isKinematic = true;
+        GetComponent<ParticleSystem>().Play();
+    }
+
+    void Start() {
+
+    }
+
+    void Update() {
+        
+    }
+}
---
+++ Unity Practice/day005-190102(수)/Bamsongi/Assets/Scripts/BamsongiGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BamsongiGenerator : MonoBehaviour {
+    public GameObject bamsongiPrefab;
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            GameObject bamsongi = Instantiate(bamsongiPrefab) as GameObject;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 worldDir = ray.direction;
+            bamsongi.GetComponent<BamsongiController>().Shoot(worldDir.normalized * 2000);
+        }
+    }
+}
