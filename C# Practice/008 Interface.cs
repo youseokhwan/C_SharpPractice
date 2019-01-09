f1369de6481e4c2e6035f6bb496909ee21cb0958
youseokhwan <tjrghks115@naver.com>
2019-01-09 23:51:58 +0900

    Practice C#: Interface

---
+++ C# Practice/008 Interface.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace C_SharpPractice {
+    interface IAnimal {
+        void WriteVoice();
+    }
+
+    class Dog : IAnimal {
+        public void WriteVoice() {
+            Console.WriteLine("멍멍");
+        }
+    }
+    
+    class Cat : IAnimal {
+        public void WriteVoice() {
+            Console.WriteLine("야옹");
+        }
+    }
+
+    class Interface {
+        public static void Main(String[] args) {
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            dog.WriteVoice();
+            cat.WriteVoice();
+        }
+    }
+}
