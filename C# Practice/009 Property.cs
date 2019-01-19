c5b1c1185305da34829b49b56fb1ed66f267c6c4
youseokhwan <tjrghks115@naver.com>
2019-01-19 15:12:39 +0900

    Practice C#: Property

---
+++ C# Practice/009 Property.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace C_SharpPractice {
+    class Animal {
+        private int num;
+        private string name;
+
+        public int Num { get; set; }
+        public string Name { get; set; }
+    }
+
+    class Property {
+        static void Main(String[] args) {
+            Animal cat = new Animal();
+            cat.Num = 1;
+            cat.Name = "Cat";
+            Console.WriteLine($"num: {cat.Num}, name: {cat.Name}");
+
+            Animal dog = new Animal() {
+                Num = 2, Name = "Dog"
+            };
+            Console.WriteLine($"num: {dog.Num}, name: {dog.Name}");
+        }
+    }
+}
