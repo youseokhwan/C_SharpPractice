90b3d4864d068367a1abbd91a825fc4fbc95d094
youseokhwan <tjrghks115@naver.com>
2019-01-28 19:33:34 +0900

    Practice C#: Collection

---
+++ C# Practice/011 Collection.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+
+namespace C_SharpPractice {
+    class Collection {
+        static void Main(String[] args) {
+            // ArrayList
+            ArrayList list = new ArrayList();
+
+            for(int i = 0; i < 5; i++) {
+                list.Add(i);
+            }
+
+            foreach(var i in list) {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine();
+
+            list.RemoveAt(1);
+            list.Insert(1, 25);
+
+            foreach (var i in list) {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine();
+
+            // Queue
+            Queue que = new Queue();
+            que.Enqueue(1);
+            que.Enqueue(2);
+
+            Console.Write($"{que.Dequeue()} ");
+            Console.Write($"{que.Dequeue()} ");
+            Console.WriteLine();
+
+            // Stack
+            int[] arr = { 1, 2, 3 };
+            Stack stk = new Stack(arr);
+
+            while(stk.Count > 0) {
+                Console.Write($"{stk.Pop()} ");
+            }
+            Console.WriteLine();
+
+            // Hashtable
+            Hashtable hash = new Hashtable();
+            hash["하나"] = 1;
+            hash["둘"] = 2;
+
+            Console.WriteLine($"{hash["하나"]}");
+        }
+    }
+}
