d869a2b4afbe7a19930d1249b8887dc80b65c12d
youseokhwan <tjrghks115@naver.com>
2019-12-03 12:56:36 +0900

    Vuejs 코딩 공작소 ch6, 7 일부

---
+++ Vuejs/Vuejs 코딩 공작소/6-07-single-file-component.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="hello">
+        {{msg}}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'hello',
+    data() {
+        return {
+            msg: 'Vue.js 앱에 오신 걸 환영합니다'
+        }
+    }
+};
+</script>
+
+<style scoped>
+</style>
