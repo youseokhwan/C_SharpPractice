31c87a26793dc50e9bb88248b4d6ac0b11e3ac55
youseokhwan <tjrghks115@naver.com>
2019-12-04 22:02:15 +0900

    Vue.js: bxuip의 button, img, progress 클론 코딩

---
+++ Vuejs/bxuip-clone/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="app">
+    <h2>Button 컴포넌트</h2>
+    <h3>Action</h3>
+    <gb-button label="default"></gb-button>
+    <gb-button label="Button" action="button"></gb-button>
+    <gb-button label="Reset" action="reset"></gb-button>
+    <gb-button label="Submit" action="submit"></gb-button>
+
+    <h3>ColorLevel</h3>
+    <gb-button label="ColorLevel 1" colorLevel="colorLevel1"></gb-button>
+    <gb-button label="ColorLevel 2" colorLevel="colorLevel2"></gb-button>
+    <gb-button label="ColorLevel 3" colorLevel="colorLevel3"></gb-button>
+    <gb-button label="ColorLevel 4" colorLevel="colorLevel4"></gb-button>
+    <gb-button label="ColorLevel 5" colorLevel="colorLevel5"></gb-button>
+
+    <h3>Fill</h3>
+    <gb-button label="true" fill="true"></gb-button>
+    <gb-button label="false" fill="false"></gb-button>
+
+    <hr>
+
+    <h2>Img 컴포넌트</h2>
+    <h3>BtnText</h3>
+    <gb-img src="/src/assets/logo.png" width="200px" height="200px" btnText="button text"></gb-img>
+    <gb-img src="/src/assets/logo.png" width="200px" height="200px" btnText="button" caption="caption"></gb-img>
+
+    <h3>Caption</h3>
+    <gb-img src="/src/assets/g-bridge.png" width="240px" height="240px" caption="캡션입니다"></gb-img>
+
+    <h3>Desc</h3>
+    <gb-img src="/src/assets/logo.png" width="200px" height="200px" desc="이미지 설명입니다"></gb-img>
+
+    <hr>
+
+    <h2>Progress 컴포넌트</h2>
+    <h3>Bar</h3>
+    <gb-progress label="wave" value="80" max="100"></gb-progress>
+
+    <h3>Color</h3>
+    <gb-progress label="red" value="55" max="100" color="red"></gb-progress>
+    <gb-progress label="blue" value="20" max="100" color="blue"></gb-progress>
+    <gb-progress label="green" value="97" max="100" color="green"></gb-progress>
+    <gb-progress label="#0d918c" value="40" max="100" color="#0d918c"></gb-progress>
+    <gb-progress label="#148fd7" value="87" max="100" color="#148fd7"></gb-progress>
+  </div>
+</template>
+
+<script>
+  import GbButton from './components/GbButton'
+  import GbImg from './components/GbImg'
+  import GbProgress from './components/GbProgress'
+
+  export default {
+    components: {
+      GbButton,
+      GbImg,
+      GbProgress
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
+ 
---
+++ Vuejs/bxuip-clone/components/GbButton.vue
@@ -0,0 +1,53 @@
+<template>
+    <span>
+        <button class="gb-button" :action="action" :class="[colorLevel, fill]">{{label}}</button>
+    </span>
+</template>
+
+<script>
+export default {
+    props: {
+        label: String,
+        action: String,
+        colorLevel: String,
+        fill: String
+    }
+}
+</script>
+
+<style scoped>
+    .gb-button {
+        box-sizing: border-box;
+        height: 25px;
+        line-height: 25px;
+        cursor: pointer;
+        display: inline-block;
+        border: 0;
+        padding: 0 10px;
+        border-radius: 3px;
+        font-size: 11px;
+        text-transform: capitalize;
+        color: white;
+        background: #1297bf;
+    }
+    .colorLevel1 {
+        background: hsla(178,83%,31%,1);
+    }
+    .colorLevel2 {
+        background: hsla(186,83%,36%,1);
+    }
+    .colorLevel3 {
+        background: hsla(194,83%,41%,1);
+    }
+    .colorLevel4 {
+        background: hsla(202,83%,46%,1);
+    }
+    .colorLevel5 {
+        background: hsla(210,83%,51%,1);
+    }
+    .true {
+        background: #ffffff;
+        color: hsla(194,83%,41%,1);
+        box-shadow:inset 0 0 0 1px hsla(194,83%,41%,1)
+    }
+</style>
