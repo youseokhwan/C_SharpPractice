9d35b24908e84e5277440dc1bfbe18f09464a91c
youseokhwan <tjrghks115@naver.com>
2020-01-25 22:42:51 +0900

    Spring Boot: board 프로젝트 추가, 수정, 삭제 구현

--- Spring Boot/board/src/main/java/board/configuration/DatabaseConfiguration.java
+++ Spring Boot/board/src/main/java/board/configuration/DatabaseConfiguration.java
@@ -34,10 +34,18 @@ public class DatabaseConfiguration {
 	}
 	
 	@Bean
+	@ConfigurationProperties(prefix="mybatis.configuration")
+	public org.apache.ibatis.session.Configuration mybatisConfig() {
+		return new org.apache.ibatis.session.Configuration();
+	}
+	
+	@Bean
 	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
 		SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
 		sqlSessionFactoryBean.setDataSource(dataSource);
 		sqlSessionFactoryBean.setMapperLocations(applicationContext.getResources("classpath:/mapper/**/sql-*.xml"));
+		sqlSessionFactoryBean.setConfiguration(mybatisConfig());
+		
 		return sqlSessionFactoryBean.getObject();
 	}
 	
@@ -45,4 +53,5 @@ public class DatabaseConfiguration {
 	public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
 		return new SqlSessionTemplate(sqlSessionFactory);
 	}
+	
 }
