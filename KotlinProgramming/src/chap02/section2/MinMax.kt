c770572fc73bde859b170d882c39691472c89814
youseokhwan <tjrghks115@naver.com>
2020-02-12 23:17:33 +0900

    Kotlin: Package, Data Type, Variable

---
+++ KotlinProgramming/src/chap02/section2/MinMax.kt
@@ -0,0 +1,8 @@
+package chap02.section2
+
+fun main() {
+    println("Int min: ${Int.MIN_VALUE}")
+    println("Int max: ${Int.MAX_VALUE}")
+    println("Double min: ${Double.MIN_VALUE}")
+    println("Double max: ${Double.MAX_VALUE}")
+}
---
+++ KotlinProgramming/src/chap02/section2/ValVar.kt
@@ -0,0 +1,12 @@
+package chap02.section2
+
+fun main() {
+    val number = 10             // val 키워드는 상수
+    var language = "Korean"     // var 키워드는 변수
+    val secondNumber: Int = 20
+    language = "English"
+
+    println("number: $number")
+    println("language: $language")
+    println("secondNumber: $secondNumber")
+}
