10d7141fad8b54c239b0b3f015ef3d8163573c10
youseokhwan <tjrghks115@naver.com>
2020-03-04 21:55:20 +0900

    Kotlin: 배열, 문자열

---
+++ Kotlin/KotlinProgramming/src/chap08/section2/SortByMap.kt
@@ -0,0 +1,10 @@
+package chap08.section2
+
+fun main() {
+    val fruits = arrayOf("banana", "avocado", "apple", "kiwi")
+    fruits
+        .filter { it.startsWith("a") }
+        .sortedBy { it }
+        .map { it.toUpperCase() }
+        .forEach { println(it) }
+}
