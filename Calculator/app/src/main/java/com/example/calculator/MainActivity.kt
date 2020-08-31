a13bd6268c3216e5dba4d0218ea722ab4d39ddf6
youseokhwan <tjrghks115@naver.com>
2020-08-31 16:25:39 +0900

    버튼 tooltip 설정

--- Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,13 +2,10 @@ package com.example.calculator
 
 import android.content.Intent
 import android.content.res.Configuration
-import android.graphics.drawable.GradientDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,8 +20,8 @@ class MainActivity : AppCompatActivity() {
         edtExpression.setTextIsSelectable(true)
         edtExpression.showSoftInputOnFocus = false
 
-        // btnConverter Click 이벤트
-        btnConverter.setOnClickListener {
+        // btnUnitConverter Click 이벤트
+        btnUnitConverter.setOnClickListener {
             // 단위 계산기 Activity 호출
             startActivity(Intent(this, UnitConverterActivity::class.java))
         }
@@ -36,37 +33,37 @@ class MainActivity : AppCompatActivity() {
     // 기기의 Orientation에 따른 Button Visibility 설정
     private fun setButtonsVisibility() {
         if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            btnMainSwitch .visibility = View.VISIBLE
-            btnMainRad    .visibility = View.VISIBLE
-            btnMainRoot   .visibility = View.VISIBLE
-            btnMainSin    .visibility = View.VISIBLE
-            btnMainCos    .visibility = View.VISIBLE
-            btnMainTan    .visibility = View.VISIBLE
-            btnMainLn     .visibility = View.VISIBLE
-            btnMainLog    .visibility = View.VISIBLE
-            btnMainInverse.visibility = View.VISIBLE
-            btnMainESupX  .visibility = View.VISIBLE
-            btnMainXSup2  .visibility = View.VISIBLE
-            btnMainXSupY  .visibility = View.VISIBLE
-            btnMainAbs    .visibility = View.VISIBLE
-            btnMainPi     .visibility = View.VISIBLE
-            btnMainEuler  .visibility = View.VISIBLE
+            btnMainSwitch.visibility = View.VISIBLE
+            btnMainRad   .visibility = View.VISIBLE
+            btnMainRoot  .visibility = View.VISIBLE
+            btnMainSin   .visibility = View.VISIBLE
+            btnMainCos   .visibility = View.VISIBLE
+            btnMainTan   .visibility = View.VISIBLE
+            btnMainLn    .visibility = View.VISIBLE
+            btnMainLog   .visibility = View.VISIBLE
+            btnMainInv   .visibility = View.VISIBLE
+            btnMainESupX .visibility = View.VISIBLE
+            btnMainXSup2 .visibility = View.VISIBLE
+            btnMainXSupY .visibility = View.VISIBLE
+            btnMainAbs   .visibility = View.VISIBLE
+            btnMainPi    .visibility = View.VISIBLE
+            btnMainEuler .visibility = View.VISIBLE
         } else {
-            btnMainSwitch .visibility = View.GONE
-            btnMainRad    .visibility = View.GONE
-            btnMainRoot   .visibility = View.GONE
-            btnMainSin    .visibility = View.GONE
-            btnMainCos    .visibility = View.GONE
-            btnMainTan    .visibility = View.GONE
-            btnMainLn     .visibility = View.GONE
-            btnMainLog    .visibility = View.GONE
-            btnMainInverse.visibility = View.GONE
-            btnMainESupX  .visibility = View.GONE
-            btnMainXSup2  .visibility = View.GONE
-            btnMainXSupY  .visibility = View.GONE
-            btnMainAbs    .visibility = View.GONE
-            btnMainPi     .visibility = View.GONE
-            btnMainEuler  .visibility = View.GONE
+            btnMainSwitch.visibility = View.GONE
+            btnMainRad   .visibility = View.GONE
+            btnMainRoot  .visibility = View.GONE
+            btnMainSin   .visibility = View.GONE
+            btnMainCos   .visibility = View.GONE
+            btnMainTan   .visibility = View.GONE
+            btnMainLn    .visibility = View.GONE
+            btnMainLog   .visibility = View.GONE
+            btnMainInv   .visibility = View.GONE
+            btnMainESupX .visibility = View.GONE
+            btnMainXSup2 .visibility = View.GONE
+            btnMainXSupY .visibility = View.GONE
+            btnMainAbs   .visibility = View.GONE
+            btnMainPi    .visibility = View.GONE
+            btnMainEuler .visibility = View.GONE
         }
     }
 }
