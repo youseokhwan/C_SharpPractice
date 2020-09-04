6676d6d7a4180f85e3ca023e5cf3c1224ef84717
yuyw0712 <yuyw0712@naver.com>
2020-09-05 02:55:23 +0900

    단위를 이름과 기호로 분리 후 통합 및 string.xml 수정

--- Calculator/app/src/main/java/com/example/calculator/UnitConverterActivity.kt
+++ Calculator/app/src/main/java/com/example/calculator/UnitConverterActivity.kt
@@ -35,13 +35,59 @@ class UnitConverterActivity : AppCompatActivity() {
         val speedList       = resources.getStringArray(R.array.spinnerArraySpeed      ).toMutableList() // 속도
         val timeList        = resources.getStringArray(R.array.spinnerArrayTime       ).toMutableList() // 시간
 
+        // 단위를 위한 리스트
+        val areaSignList        = resources.getStringArray(R.array.spinnerArrayAreaSign       ).toMutableList() // 면적단위
+        val lengthSignList      = resources.getStringArray(R.array.spinnerArrayLengthSign     ).toMutableList() // 길이단위
+        val temperatureSignList = resources.getStringArray(R.array.spinnerArrayTemperatureSign).toMutableList() // 온도단위
+        val volumeSignList      = resources.getStringArray(R.array.spinnerArrayVolumeSign     ).toMutableList() // 부피단위
+        val massSignList        = resources.getStringArray(R.array.spinnerArrayMassSign       ).toMutableList() // 무게단위
+        val dataSignList        = resources.getStringArray(R.array.spinnerArrayDataSign       ).toMutableList() // 데이터단위
+        val speedSignList       = resources.getStringArray(R.array.spinnerArraySpeedSign      ).toMutableList() // 속도단위
+        val timeSignList        = resources.getStringArray(R.array.spinnerArrayTimeSign       ).toMutableList() // 시간단위
+
+        // 이름 + 단위 리스트
+        val areaListAll = mutableListOf<String>()
+        val lengthListAll = mutableListOf<String>()
+        val temperatureListAll = mutableListOf<String>()
+        val volumeListAll = mutableListOf<String>()
+        val massListAll = mutableListOf<String>()
+        val dataListAll = mutableListOf<String>()
+        val speedListAll = mutableListOf<String>()
+        val timeListAll = mutableListOf<String>()
+
+        // 이름 + 단위를 더하는 작업
+        for(i in areaList.indices) {
+            areaListAll.add(areaList[i] + " (" + areaSignList[i]+")")
+        }
+        for(i in lengthList.indices) {
+            lengthListAll.add(lengthList[i] + " (" + lengthSignList[i]+")")
+        }
+        for(i in temperatureList.indices) {
+            temperatureListAll.add(temperatureList[i] + " (" + temperatureSignList[i]+")")
+        }
+        for(i in volumeList.indices) {
+            volumeListAll.add(volumeList[i] + " (" + volumeSignList[i]+")")
+        }
+        for(i in massList.indices) {
+            massListAll.add(massList[i] + " (" + massSignList[i]+")")
+        }
+        for(i in dataList.indices) {
+            dataListAll.add(dataList[i] + " (" + dataSignList[i]+")")
+        }
+        for(i in speedList.indices) {
+            speedListAll.add(speedList[i] + " (" + speedSignList[i]+")")
+        }
+        for(i in timeList.indices) {
+            timeListAll.add(timeList[i] + " (" + timeSignList[i]+")")
+        }
+
         // 고유 단위 현지화
         when (Locale.getDefault().language) {
             "ko" -> { // 한국어
-                areaList  .add("평")
-                lengthList.addAll(listOf("자", "리"))
-                volumeList.addAll(listOf("말", "되"))
-                massList  .addAll(listOf("근", "돈"))
+                areaListAll  .add("평"+" (평)")
+                lengthListAll.addAll(listOf("자"+" (자)", "리"+" (리)"))
+                volumeListAll.addAll(listOf("말"+" (말)", "되"+" (되)"))
+                massListAll  .addAll(listOf("근"+" (근)", "돈"+" (돈)"))
             }
             else -> {
                 // pass
@@ -49,14 +95,14 @@ class UnitConverterActivity : AppCompatActivity() {
         }
 
         // 각 단위 리스트를 spinnerList에 추가
-        spinnerList.addAll(mutableListOf(areaList, lengthList, temperatureList, volumeList, massList, dataList, speedList, timeList))
+        spinnerList.addAll(mutableListOf(areaListAll, lengthListAll, temperatureListAll, volumeListAll, massListAll, dataListAll, speedListAll, timeListAll))
 
         // 변수 Adapter에 ViewPagerAdapter를 객체화
         val adapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
 
         // 각 단위 list를 adapter에 추가
         for (unitList in spinnerList) {
-            adapter.addFragment(UnitFragment(unitList))
+            adapter.addFragment(UnitFragment((unitList)))
         }
 
         viewPager.adapter = adapter
