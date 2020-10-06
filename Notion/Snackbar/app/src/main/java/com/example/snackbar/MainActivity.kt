61594b9a236c09e0b8ad83650cb4284e4c8221cf
youseokhwan <tjrghks115@naver.com>
2020-10-06 10:57:05 +0900

    Blog: Snackbar

---
+++ Notion/Snackbar/app/src/main/java/com/example/snackbar/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.snackbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            Snackbar.make(coordinatorLayout, "Snackbar Test!!", Snackbar.LENGTH_SHORT)
+                    .setAction("action", View.OnClickListener {
+                        Log.d("SnackbarLog", "action 클릭됨!!")
+                    })
+                    .show()
+        }
+    }
+}
