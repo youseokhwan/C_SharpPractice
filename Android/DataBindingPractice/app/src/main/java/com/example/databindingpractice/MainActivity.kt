76f6a96e3acb801fa977887ae446d7a20c3d7b5d
youseokhwan <tjrghks115@naver.com>
2020-12-29 13:36:53 +0900

    Android: DataBindingPractice

---
+++ Android/DataBindingPractice/app/src/main/java/com/example/databindingpractice/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.databindingpractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.databindingpractice.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.user = User("Seokhwan", "You")
+    }
+}
